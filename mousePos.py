b985c06a474c3a2dbf525e63df6b0a5f51f44b9e
evans222 <wookieman088@gmail.com>
2022-10-28 12:16:25 -0600

    dfs

--- mousePos.py
+++ mousePos.py
@@ -1,9 +1,21 @@
+import win32gui
 import pyautogui
+
+def get_pixel_colour(i_x, i_y):
+	i_desktop_window_id = win32gui.GetDesktopWindow()
+	i_desktop_window_dc = win32gui.GetWindowDC(i_desktop_window_id)
+	long_colour = win32gui.GetPixel(i_desktop_window_dc, i_x, i_y)
+	i_colour = int(long_colour)
+	win32gui.ReleaseDC(i_desktop_window_id,i_desktop_window_dc)
+	return (i_colour & 0xff), ((i_colour >> 8) & 0xff), ((i_colour >> 16) & 0xff)
+    
+    
 print("press Ctrl+C to quit")
 try:
     while True:
         x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        a, b, c = get_pixel_colour(x, y)
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4) + ' color: ' + str(a).rjust(3) + ', ' + str(b).rjust(3) + ', ' + str(c).rjust(3)
         print(positionStr, end='')
         print('\b' * len(positionStr), end='', flush=True)
 
